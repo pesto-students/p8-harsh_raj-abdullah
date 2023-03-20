622894ab89f8dd6f888ec07b4b5451f6299f30b1
Abdul7872 <aabdull787275@gmail.com>
2023-03-20 17:20:11 +0530

    Dev - API integrate, btn logic and design added.

--- Week-13/url-shortener/src/App.js
+++ Week-13/url-shortener/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />}  />
-        <Route path='/pl' element={<h1>H1</h1>}  />
-        <Route path='/p/l' element={<h1>H2</h1>}  />
+        <Route path='/' element={<Layout/>} >
+          <Route index element={<Home />}  />
+          <Route path='/feature' element={<h1>This is features page.</h1>}  />
+          <Route path='/pricing' element={<h1>This is pricing page.</h1>}  />
+          <Route path='/resources' element={<h1>This is resources page.</h1>}  />
+          <Route path='/login' element={<h1>This is login page.</h1>}  />
+          <Route path='/signup' element={<h1>This is signup page.</h1>}  />
+        </Route>
         <Route path='*' element={<h1>No Page Found!</h1>}  />
-
       </Routes>
     </BrowserRouter>
   );
--- Week-13/url-shortener/src/pages/Home.js
+++ Week-13/url-shortener/src/pages/Home.js
@@ -1,66 +1,64 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import './Home.css'
 
+const BASE_URL = 'https://api.shrtco.de/v2/shorten'
+
 function Home () {
+  const [loading, setLoading] = useState(false);
   const [url, setUrl] = useState('')
-  const onSubmit = (e) => {
+  const [urlList, setUrlList] = useState([])
+
+ 
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(url)
+    setLoading(true)
+    const response = await (await fetch(`${BASE_URL}?url=${url}`)).json()
+    if(response.ok){
+      const shortUrl = response?.result?.full_short_link
+      setUrlList([{ url, shortUrl }, ...urlList])
+      setUrl('')
+    }
+    else{
+      alert(response?.error)
+    }
+    setLoading(false)
+  }
+
+  const onClickCopy = (text) =>{
+    navigator.clipboard.writeText(text);
+    alert('Copied!')
   }
 
   return (
-    <div>
-      <header className="header">
-        <h1>Shortly</h1>
-        <div className="links">
-          <Link to={'/feature'} > Feature </Link>
-          <Link to={'/pricing'} > Pricing </Link>
-          <Link to={'/resources'} > Resources </Link>
-        </div>
-        <div className="links">
-          <Link to={'/login'} > Login </Link>
-          <Link to={'/signup'} > Sign Up </Link>
-        </div>
-      </header>
-
-      <div className="body">
-        <div>
-          <h2>More than just shorter Links</h2>
-          <div>Build your brand's recognition and get details insights on how your links are performing</div>
-        </div>
-        
-        <form className="form" onSubmit={onSubmit} >
-          <input required 
-            type="url" 
-            value={url}
-            placeholder="Shorten a link here" 
-            onChange={e => setUrl(e.target.value)} 
-          />
-          <button type="submit">Shorten It!</button>
-        </form>
-
-        <div className="shorter_box">
-          <div className="shorter_link_holder">
-            <span>kjdbosbd</span>
-            <span>lksdnls <button>COPY</button></span>
-          </div>
-        </div>
-
+    <div className="body">
+      <div>
+        <h2>More than just shorter Links</h2>
+        <div>Build your brand's recognition and get details insights on how your links are performing</div>
       </div>
+      
+      <form className="form" onSubmit={onSubmit} >
+        <input required 
+          type="url" 
+          value={url}
+          placeholder="Shorten a link here" 
+          onChange={e => setUrl(e.target.value)} 
+        />
+        <button type="submit">{loading ? '.....' : 'Shorten It!'}</button>
+      </form>
 
-      <footer className="footer">
-        <h1>Shortly</h1>
-        <div className="links">
-          <Link to={'/feature'} > Feature </Link>
-          <Link to={'/pricing'} > Pricing </Link>
-          <Link to={'/resources'} > Resources </Link>
-        </div>
-        <div className="links">
-          <Link to={'/login'} > Login </Link>
-          <Link to={'/signup'} > Sign Up </Link>
-        </div>
-      </footer>
+      <div className="url_list">
+        { urlList?.map((item, idx) => (
+          <div key={idx} className="shorter_box">
+            <div className="shorter_link_holder">
+              <p>{item?.url}</p>
+              <p className="shorter_link">
+                <span>{item?.shortUrl}</span>
+                <button onClick={()=>onClickCopy(item?.shortUrl)}>COPY</button>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
 
     </div>
   )
