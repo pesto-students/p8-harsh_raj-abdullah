b24bfb4b56f54df0936bcb44b101384a7c9866aa
Abdul7872 <aabdull787275@gmail.com>
2023-03-25 17:24:16 +0530

    Dev - add redux ex2 step counter design.

---
+++ Week-14/redux-ex2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StepCounter from './StepCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text'>You've walked 42 steps today!</p>
+      <StepCounter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week-14/redux-ex2/src/StepCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function StepCounter(){
+  return (
+    <div>
+      <button className="btn add">Add a Step</button>
+      <br />
+      <button className="btn reset">Reset Steps</button>
+    </div>
+  )
+}
